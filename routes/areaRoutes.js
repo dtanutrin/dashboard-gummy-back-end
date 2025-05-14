838d6054b5188ee126d9f02acdd224bf73b15d0f
Bernardo Simões <151968699+b3rnardo15@users.noreply.github.com>
2025-05-14 18:42:43 +0000

    teste

--- routes/areaRoutes.js
+++ routes/areaRoutes.js
@@ -1,8 +1,8 @@
 import express from "express";
 const router = express.Router();
 import { createArea, getAllAreas, getAreaById, updateArea, deleteArea } from "../controllers/areaController.js";
-import { authenticateToken } from "../middleware/authenticateToken.js";
-import { isAdmin } from "../middleware/isAdmin.js";
+import authenticateToken from "../middleware/authenticateToken.js"; 
+import isAdmin from "../middleware/isAdmin.js"; 
 
 // Rotas para Áreas (protegidas e apenas para admin)
 router.post("/", authenticateToken, isAdmin, createArea);
@@ -12,4 +12,3 @@ router.put("/:id", authenticateToken, isAdmin, updateArea);
 router.delete("/:id", authenticateToken, isAdmin, deleteArea);
 
 export default router;
-
